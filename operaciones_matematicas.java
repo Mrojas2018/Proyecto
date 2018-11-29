39cfe1acc1804a6b8b5b568694bd13121e03ee01
KatherineBerrios <44624787+KatherineBerrios@users.noreply.github.com>
2018-11-28 21:30:27 -0300

    Update operaciones_matematicas.java

--- operaciones_matematicas.java
+++ operaciones_matematicas.java
@@ -7,23 +7,73 @@ public class operaciones_matematicas {
 		Scanner entrada= new Scanner(System.in);
 		boolean salir=false;
 		int opcion;
-		//realiza accion, mientras no se seleccione opci髇 salir
+		//realiza accion, mientras no se seleccione opci贸n salir
 		while (!salir){
-			//realiza acci髇 mientras no se encuentren errores
+			//realiza acci贸n mientras no se encuentren errores
 			try{
-				System.out.print("Elija una opci髇"+"\n"+"	1) Sumar 2 elementos"+"\n"+
+				System.out.print("Elija una opci贸n"+"\n"+"	1) Sumar 2 elementos"+"\n"+
 						"	2) Restar 2 elementos"+"\n"+"	3) Dividir 2 elementos"+"\n"+
 						"	4) Multiplicar 2 elementos"+"\n"+"	5) Volver"+"\n");
 				opcion=entrada.nextInt();
 				switch(opcion){
-				
-				case 5:salir=true;
+						
+						case 1 : 
+						//Suma de dos elementos.
+						System.out.println("Suma de dos n煤meros.");
+				 		System.out.println();
+						System.out.print("Ingrese el primer n煤mero: ");
+						a=teclado.nextDouble();
+						System.out.print("ingrese el segundo n煤mero: ");
+						b=teclado.nextDouble();
+						respuesta=suma(a,b);
+							
+							
+						System.out.println("El resultado de la suma ("+a+"+"+b+") es: "+respuesta);
+						break;
+						
+						case 2 : 
+						//Resta de dos elementos.
+						System.out.println("Resta de dos n煤meros.");
+						System.out.println();
+						teclado = new Scanner(System.in);
+						System.out.print("Ingrese el primer n煤mero: ");
+						a = teclado.nextDouble();
+						System.out.print("Ingrese el segundo n煤mero: ");
+						b = teclado.nextDouble(); 
+						respuesta=resta(a,b);
+						System.out.println("El resultado de la resta "+a+"-"+b+" es: "+respuesta);
+						break;
+											
+						case 3: 
+						//Multiplicaci贸n de dos elementos.
+								              
+						break;
+										 
+						case 4: 
+						//Divisi贸n de dos elementos.
+										
+						break;
+										
+						case 5: 
+						//Potencia.
+						System.out.println("Potenciaci贸n de n煤meros.");
+						System.out.println();
+						teclado = new Scanner(System.in);
+						System.out.print("Ingrese la base: ");
+						a = teclado.nextDouble();
+						System.out.print("Ingrese el exponente: ");
+						b = teclado.nextDouble(); 
+						respuesta=potencia(a,b);
+						System.out.println("El resultado de la potencia de base  "+ a +" elevada a "+ b +" "+respuesta);
+						break;
+								
+						case 6: salir=true;
 						break;
 				}
-				
-			//si se ingreso un caracter distinto a un n鷐ero, o surge alg鷑 error
+								
+			//si se ingreso un caracter distinto a un n煤mero, o surge alg煤n error
 			}catch (InputMismatchException e) {
-				System.out.println("Debe insertar una opci髇 entre 1 y 5");
+				System.out.println("Debe insertar una opci贸n entre 1 y 5");
 				entrada.next();			
 			}
 			
